641f77573cf478a476ed66d5e557f31aeb64d6ed
Egogorka <bike034@mail.ru>
2023-05-22 10:15:47 +0300

    update

--- main.cpp
+++ main.cpp
@@ -5,151 +5,282 @@
 #include <fstream>
 #include <functional>
 
-//template <typename T>
-//class GridI {
+
+struct Grid {
+    double L; // Length
+    double T; // Time
+    int M; // Length discretion (amount of intervals, i.e. M+1 - amount of points)
+    int N; // Time discretion
+
+    [[nodiscard]] double h() const { return L/double(M); }
+    [[nodiscard]] double t() const { return T/double(N); }
+
+    void write(std::ostream& out) const {
+        out << L << ',' << M << ',' << T << ',' << N << '\n';
+    }
+};
+
+struct FuncGrid {
+    Grid grid{};
+    std::vector<std::vector<double>> func;
+
+    explicit FuncGrid(const Grid& grid): grid(grid), func(){
+        func.resize(grid.N+1, {});
+        for(auto& subvec: func){
+            subvec.resize(grid.M+1, {});
+        }
+    }
+
+
+    void write(std::ostream& out) const {
+        grid.write(out);
+        for(int i=0; i<=grid.N; i++){
+            for(int j=0; j<=grid.M; j++){
+                if (j != 0) out << ',';
+                out << func[i][j];
+            }
+            out << '\n';
+        }
+    }
+};
+
+//class Scheme {
+//public:
+//    /**
+//     * Atm only 0 derivative border conditions are implemented
+//     * TODO: implement other border conditions
+//     */
 //
+//    /**
+//     * @param fg - FunctionGrid to be solved. fg[0][i] must be filled with initial condition
+//     */
+//    void solve(FuncGrid& fg) = 0;
 //};
-//
-//template <typename T>
-//class Grid {
-//
-//};
-//
-//template <typename T>
-//class Interpolation {
-//
-//};
-//
-//template <typename T>
-//class GridFunction {
-//
-//    Interpolation<T>* interpolation;
-//    GridI<T>* grid
-//};
 
-
-// Сначала обусловимся на равномерную сетку
-const int M = 1000;
-const float L = 1;
-const float h = L/M;
-
-const int N = 5000;
-const float T = 1;
-const float t = T/N;
-
-const std::string PATH = "../data";
-
-float calc_f(float u){
+double calc_f(double u){
     return u*u/2;
 }
 
-// Пока - схема Куранта-Изаксона-Рис
-// Краевые условия - нулевая производная
-void progress_KIR(const std::vector<float>& u0, std::vector<float>& u1){
-    for(int i=0; i<M; i++){
-        // approximate derivative
-        auto dfdu = u0[i];
-//        if (fabs(dfdu)*t/h > 1) std::cout << "Breaching stability on x=" << float(i)*h << '\n';
-        if (dfdu > 0) {
-            if(i != 0) u1[i] = u0[i] - t/h*(calc_f(u0[i]) - calc_f(u0[i-1]));
-            else u1[i] = u0[i];
-        } else {
-            if(i != M) u1[i] = u0[i] - t/h*(calc_f(u0[i+1]) - calc_f(u0[i]));
-            else u1[i] = u0[i];
+class Scheme_KIR {
+public:
+    static void solve(FuncGrid& fg, bool border_present = false) {
+        std::cout << "KIR scheme start\n";
+        for(int j=1; j<=fg.grid.N; j++){
+            if (fg.grid.N > 10 && j % (fg.grid.N/10) == 0) std::cout << "KIR : Finished " << (j*10) / fg.grid.N * 10 << "%\n";
+            solve_layer(fg, j, border_present);
+        }
+        std::cout << "KIR scheme end\n";
+    }
+
+protected:
+    /**
+     * @param fg
+     * @param layer - layer to be filled
+     */
+    static void solve_layer(FuncGrid& fg, int layer, bool border_present = false) {
+        auto t = fg.grid.t();
+        auto h = fg.grid.h();
+        auto M = fg.grid.M;
+
+        std::vector<double>& u0 = fg.func[layer-1];
+        std::vector<double>& u1 = fg.func[layer];
+
+        if (!border_present){
+            u1[0] = u0[0];
+            u1[M] = u0[M];
+        }
+
+        for(int i=1; i<M; i++){
+            // approximate derivative
+            auto dfdu = u0[i];
+
+            // if (fabs(dfdu)*t/h > 1) std::cout << "Breaching stability on x=" << double(i)*h << '\n';
+            if (dfdu > 0) {
+                u1[i] = dfdu - t/h*(calc_f(u0[i]) - calc_f(u0[i-1]));
+            } else {
+                u1[i] = dfdu - t/h*(calc_f(u0[i+1]) - calc_f(u0[i]));
+            }
         }
     }
-}
+};
 
-// Лакса-Вендроффа
-void progress_LW(const std::vector<float>& u0, std::vector<float>& u1){
-    for(int i=0; i<M; i++){
-        //predictor
-        auto u0c = u0[i];
-        auto u0r = i != M ? u0[i+1] : u0c;
-        auto u0l = i != 0 ? u0[i-1] : u0c;
+class Scheme_LW {
+protected:
+    static void solve_layer(FuncGrid& fg, int layer, bool border_present = false) {
+        auto t = fg.grid.t();
+        auto h = fg.grid.h();
+        auto M = fg.grid.M;
 
-        auto ur = (u0c + u0r)/2 - t/2/h * (calc_f(u0r) - calc_f(u0c));
-        auto ul = (u0c + u0l)/2 - t/2/h * (calc_f(u0c) - calc_f(u0l));
-        // corrector
-        u1[i] = u0c - t/h * (calc_f(ur) - calc_f(ul));
+        std::vector<double>& u0 = fg.func[layer-1];
+        std::vector<double>& u1 = fg.func[layer];
+
+        if (!border_present){
+            u1[0] = u0[0];
+            u1[M] = u0[M];
+        }
+
+        for(int i=1; i<M; i++){
+            //predictor
+            auto u0c = u0[i];
+            auto u0r = u0[i+1];
+            auto u0l = u0[i-1];
+
+            auto ur = (u0c + u0r)/2 - t/2/h * (calc_f(u0r) - calc_f(u0c));
+            auto ul = (u0c + u0l)/2 - t/2/h * (calc_f(u0c) - calc_f(u0l));
+            // corrector
+            u1[i] = u0c - t/h * (calc_f(ur) - calc_f(ul));
+        }
+    }
+
+public:
+    static void solve(FuncGrid& fg, bool border_present = false) {
+        std::cout << "LW scheme start\n";
+        for(int j=1; j<=fg.grid.N; j++){
+            if (fg.grid.N > 10 && j % (fg.grid.N/10) == 0) std::cout << "LW : Finished " << (j*10) / fg.grid.N * 10 << "%\n";
+            solve_layer(fg, j, border_present);
+        }
+        std::cout << "LW scheme end\n";
+    }
+};
+
+
+// Сначала обусловимся на равномерную сетку
+const int M = 100;
+const double L = 1;
+
+const int N = 500;
+const double T = 1;
+
+const std::string PATH = "../data";
+
+double step(double a, double b, double x0, double x, double t){
+    if (a < b) {
+        if (x < x0 + a * t) return a;
+        if (x >= x0 + b * t) return b;
+        return (x - x0) / t;
+    } else {
+        if (x < x0 + (a+b)/2 * t) return a;
+        return b;
     }
 }
 
-void write(std::ofstream& fout, const std::vector<float>& u, float time){
-    fout << time;
-    for(int i=0; i<M; i++){
-        fout << ',' << u[i];
-    }
-    fout << '\n';
+/**
+ * @param a = u(x=0, t=0)
+ * @param b = u(x=L, t=0)
+ * @param x
+ * @param t
+ * @return u(x,t)
+ */
+double line(double a, double b, double L, double x, double t){
+    return (L*a + (b-a)*x)/(L+(b-a)*t);
 }
 
-void write2file(std::vector<float> u0, // we need copy there
-                const std::function<void(const std::vector<float>&, std::vector<float>&)>& solver,
-                const std::string& path)
-{
+double soliton(double C, double x, double t){
+    auto w = x/sqrt(t);
+    return (w + sqrt(w*w - 4*C))/2/sqrt(t);
+}
+
+
+void write2file(const std::string& path, const std::function<void(std::ofstream&)>& f){
     std::ofstream fout;
     fout.open(PATH + path);
-
-    std::vector<float> u1; // buffer
-    u1.resize(M);
-
-//    std::cout << "-- Filling x-es top row\n";
-    fout << 0;
-    for (int i = 0; i < M; i++)
-        fout << ',' << i * h;
-    fout << '\n';
-
-    for(int j=0; (2*j)<N; j++){
-        if (j % (N*5/200) == 0) std::cout << (j / (N/200)) << "%\n";
-        solver(u0, u1);
-        write(fout, u1, float(2*j)*t);
-        solver(u1, u0);
-        write(fout, u0, float(2*j+1)*t);
-    }
-
+    f(fout);
     fout.close();
 }
 
+void calc_func(const Grid& grid, const std::string& path, const std::function<double(double,double)>& f, bool border = false){
+    std::cout << "Step-up calculation for " << path << "\n";
+    std::cout << "Grid parameters:";
+    grid.write(std::cout);
+    FuncGrid fg0(grid), fg_KIR(grid), fg_LW(grid);
+
+    // Filling the analitycal solution
+    for(int j=0; j<=grid.N; j++)
+        for(int i=0; i<=grid.M; i++)
+//            fg0.func[j][i] = step(1, 2, 0.5, double(i)*grid.h(), double(j)*grid.t());
+            fg0.func[j][i] = f(double(i)*grid.h(), double(j)*grid.t());
+
+    // Filling initial condition
+    for(int i=0; i<=grid.M; i++)
+        fg_LW.func[0][i] = fg_KIR.func[0][i] = f(double(i)*grid.h(), 0);
+
+    if (border) {
+        for (int i = 0; i <= grid.N; i++) {
+            fg_LW.func[i][0] = fg_KIR.func[i][0] = f(0, double(i)*grid.t());
+            fg_LW.func[i][grid.M] = fg_KIR.func[i][grid.M] = f(grid.L, double(i)*grid.t());
+        }
+    }
+
+    Scheme_KIR::solve(fg_KIR, border);
+    Scheme_LW::solve(fg_LW, border);
+
+    write2file(path+"_dataKIR.csv", [&](std::ofstream& fout){fg_KIR.write(fout);});
+    write2file(path+"_dataLW.csv", [&](std::ofstream& fout){fg_LW.write(fout);});
+    write2file(path+"_dataAnalytic.csv", [&](std::ofstream& fout){fg0.write(fout);});
+}
+
 int main() {
-    std::vector<float> u0; // input
 
-    u0.resize(M);
+    auto step_up = [&](double x, double t){
+        return step(1, 2, 0.5, x, t);
+    };
 
-    std::cout << "Line-up calculation\n";
+    auto line_up = [&](double x, double t){
+        return step(1, 2, - 1*1.f/(2-1), x, t + 1.f/(2-1));
+    };
 
-    for (int i = 0; i < M; i++)
-        u0[i] = 1 + i * h;
+    auto soliton_up = [&](double x, double t) {
+        return soliton(-0.1, x, t+1.1f);
+    };
 
-    write2file(u0, progress_KIR, "/line_up/dataKIR.csv");
-    write2file(u0, progress_LW, "/line_up/dataLW.csv");
+//    step_up_calc({.L = 1, .T = 0.5, .M = 10, .N = 50}, "/stepup/1e1.0");
+//    step_up_calc({.L = 1, .T = 0.5, .M = 20, .N = 100}, "/stepup/1e1.3");
+//    step_up_calc({.L = 1, .T = 0.5, .M = 50, .N = 250}, "/stepup/1e1.6");
+//    step_up_calc({.L = 1, .T = 0.5, .M = 100, .N = 500}, "/stepup/1e2.0");
+//    step_up_calc({.L = 1, .T = 0.5, .M = 200, .N = 1000}, "/stepup/1e2.3");
+//    step_up_calc({.L = 1, .T = 0.5, .M = 500, .N = 2500}, "/stepup/1e2.6");
+//    step_up_calc({.L = 1, .T = 0.5, .M = 1000, .N = 5000}, "/stepup/1e3.0");
+//    step_up_calc({.L = 1, .T = 0.5, .M = 2000, .N = 10000}, "/stepup/1e3.3");
+//    step_up_calc({.L = 1, .T = 0.5, .M = 5000, .N = 25000}, "/stepup/1e3.6");
 
-    std::cout << "Sin calculation\n";
+//    calc_func({.L = 1, .T = 0.5, .M = 10, .N = 50}, "/line_up/1e1.0", line_up);
+//    calc_func({.L = 1, .T = 0.5, .M = 20, .N = 100}, "/line_up/1e1.3", line_up);
+//    calc_func({.L = 1, .T = 0.5, .M = 50, .N = 250}, "/line_up/1e1.6", line_up);
+//    calc_func({.L = 1, .T = 0.5, .M = 100, .N = 500}, "/line_up/1e2.0", line_up);
+//    calc_func({.L = 1, .T = 0.5, .M = 200, .N = 1000}, "/line_up/1e2.3", line_up);
+//    calc_func({.L = 1, .T = 0.5, .M = 500, .N = 2500}, "/line_up/1e2.6", line_up);
+//    calc_func({.L = 1, .T = 0.5, .M = 1000, .N = 5000}, "/line_up/1e3.0", line_up);
 
-    for (int i = 0; i < M; i++)
-        u0[i] = 1 + 2.f/3 * sinf(M_PI * 2 * i / M);
+    calc_func({.L = 1, .T = 0.5, .M = 2, .N = 3}, "/soliton_up/1e0.3", soliton_up, true);
+    calc_func({.L = 1, .T = 0.5, .M = 5, .N = 8}, "/soliton_up/1e0.6", soliton_up, true);
+    calc_func({.L = 1, .T = 0.5, .M = 10, .N = 15}, "/soliton_up/1e1.0", soliton_up, true);
+    calc_func({.L = 1, .T = 0.5, .M = 20, .N = 30}, "/soliton_up/1e1.3", soliton_up, true);
+    calc_func({.L = 1, .T = 0.5, .M = 50, .N = 75}, "/soliton_up/1e1.6", soliton_up, true);
+    calc_func({.L = 1, .T = 0.5, .M = 100, .N = 150}, "/soliton_up/1e2.0", soliton_up, true);
+    calc_func({.L = 1, .T = 0.5, .M = 200, .N = 300}, "/soliton_up/1e2.3", soliton_up, true);
+    calc_func({.L = 1, .T = 0.5, .M = 500, .N = 750}, "/soliton_up/1e2.6", soliton_up, true);
+    calc_func({.L = 1, .T = 0.5, .M = 1000, .N = 1500}, "/soliton_up/1e3.0", soliton_up, true);
 
-    write2file(u0, progress_KIR, "/sin/dataKIR.csv");
-    write2file(u0, progress_LW, "/sin/dataLW.csv");
-
-
-    std::cout << "Step-up calculation\n";
-
-    for (int i = 0; i < M; i++)
-        if (2*i > M) u0[i] = 2;
-        else u0[i] = 1;
-
-    write2file(u0, progress_KIR, "/stepup/dataKIR.csv");
-    write2file(u0, progress_LW, "/stepup/dataLW.csv");
+//    calc_func({.L = 1, .T = 0.5, .M = 2000, .N = 10000}, "/line_up/1e3.3", line_up);
+//    calc_func({.L = 1, .T = 0.5, .M = 5000, .N = 25000}, "/line_up/1e3.6", line_up);
 
 
-    std::cout << "Step-down calculation\n";
 
-    for (int i = 0; i < M; i++)
-        if (2*i < M) u0[i] = 2;
-        else u0[i] = 1;
 
-    write2file(u0, progress_KIR, "/stepdown/dataKIR.csv");
-    write2file(u0, progress_LW, "/stepdown/dataLW.csv");
+//    for (int i = 0; i < M; i++)
+//        u0[i] = step(1, 2, 0.5f, h*double(i), 0);
+//
+//    write2file(u0, progress_KIR, "/stepup/10_3dataKIR.csv");
+//    write2file(u0, progress_LW, "/stepup/10_3dataLW.csv");
+//    write2file()
+//
+//    std::cout << "Step-down calculation\n";
+//
+//    for (int i = 0; i < M; i++)
+//        u0[i] = step(2, 1, 0.5f, h*double(i), 0);
+//
+//    write2file(u0, progress_KIR, "/stepdown/10_3dataKIR.csv");
+//    write2file(u0, progress_LW, "/stepdown/10_3dataLW.csv");
 
     return 0;
 }
--- show.py
+++ show.py
@@ -1,39 +1,389 @@
+import math
+import os, glob
+
 import matplotlib.pyplot as plt
 import numpy as np
 
 time = 1
+data_arr = {}
+multiplier = 1
+T = 0.5
 
 fig, ax = plt.subplots(figsize=(15,15))
 
 def on_press(event):
     global time
-    global data
+    global data_arr
+    global multiplier
+
     if event.key == "e":
-        time = time + 50
+        time = time + 1
     if event.key == "w":
-        time = time - 50
+        time = time - 1
     ax.clear()
-    ax.set_xlim(-1, 2+1000)
-    ax.set_ylim(0.5, 2.5)
-    ax.plot(data[time])
-    ax.set_title(f'Time t= {time/5000}')
+    ax.set_xlim(-0.1, 1.1)
+    ax.set_ylim(-0.1, 1.1)
+    for key in data_arr:
+        print(key)
+        fg = data_arr[key]
+        n = fg.grid['N']
+        print(fg.get_x())
+        ax.plot(fg.get_x(), fg.data[int(time*n/multiplier)])
+    ax.set_title(f'Time t= {time*T/multiplier}')
     # [l.set_visible(False) for (i,l) in enumerate(ax.xaxis.get_ticklabels()) if i % 100 != 0]
     fig.canvas.draw()
 
 fig.canvas.mpl_connect('key_press_event', on_press)
 
+
+class FuncGrid:
+
+    @staticmethod
+    def get_grid(path):
+        f = open(path)
+        x = f.readline().split(',')
+        f.close()
+        return {
+            'L': float(x[0]),
+            'M': int(x[1]),
+            'T': float(x[2]),
+            'N': int(x[3]),
+            'h': float(x[0])/float(x[1]),
+            't': float(x[2])/float(x[3])
+        }
+
+    def get_x(self):
+        return [i*self.grid['h'] for i in range(self.grid['M']+1)]
+
+    def get_t(self):
+        return [j*self.grid['t'] for j in range(self.grid['N']+1)]
+
+    def __init__(self, path):
+        print("Loading ", path)
+        self.grid = FuncGrid.get_grid(path)
+        self.data = np.loadtxt(path, delimiter=',', skiprows=1)
+
+def step_up_show():
+    global data_arr
+    global multiplier
+    global T
+
+    T = data_arr['analytic'].grid['T']
+    multiplier = 0
+    for key in data_arr:
+        n = data_arr[key].grid['N']
+        if multiplier == 0:
+            multiplier = n
+        multiplier = math.gcd(multiplier, n)
+
+
+def calc_dif(fg1, arr_fg):
+    n = fg1.grid['N']
+    for key in arr_fg:
+        n = math.gcd(arr_fg[key].grid['N'], n)
+
+    n_fg1 = fg1.grid['N']
+    m_fg1 = fg1.grid['M']
+    l1 = {}
+    l2 = {}
+    linf = {}
+    times = []
+    for key in arr_fg:
+        l1[key] = []
+        l2[key] = []
+        linf[key] = []
+
+    for i in range(n):
+        times.append(i*T/n)
+
+        for key in arr_fg:
+            fg = arr_fg[key]
+            n_cur = fg.grid['N']
+            m_cur = fg.grid['M']
+            m = math.gcd(m_cur, m_fg1)
+
+            l1[key].append(np.sum([np.abs(fg.data[int(i*n_cur/n)][int(j*m_cur/m)] - fg1.data[int(i*n_fg1/n)][int(j*m_fg1/m)]) for j in range(m)])/m)
+            l2[key].append((np.sum([np.abs(fg.data[int(i*n_cur/n)][int(j*m_cur/m)] - fg1.data[int(i*n_fg1/n)][int(j*m_fg1/m)])**2 for j in range(m)])/m)**(1/2))
+            linf[key].append(np.max([np.abs(fg.data[int(i*n_cur/n)][int(j*m_cur/m)] - fg1.data[int(i*n_fg1/n)][int(j*m_fg1/m)]) for j in range(m)]))
+
+    return l1, linf, l2, times
+
+def calc_dif_point(fg1, arr_fg):
+    n = fg1.grid['N']
+    m = fg1.grid['M']
+    for key in arr_fg:
+        n = math.gcd(arr_fg[key].grid['N'], n)
+        m = math.gcd(arr_fg[key].grid['M'], m)
+
+    n_fg1 = fg1.grid['N']
+    m_fg1 = fg1.grid['M']
+    dif = {}
+    times = []
+    pos = []
+    for key in arr_fg:
+        dif[key] = np.zeros(shape=(n, m))
+
+    for i in range(n):
+        times.append(i*T/n)
+        for j in range(m):
+            pos.append(j*1/m)
+
+            for key in arr_fg:
+                fg = arr_fg[key]
+                n_cur = fg.grid['N']
+                m_cur = fg.grid['M']
+
+                dif[key][i][j] = np.abs(fg.data[int(i*n_cur/n)][int(j*m_cur/m)] - fg1.data[int(i*n_fg1/n)][int(j*m_fg1/m)])
+
+    return dif, times, pos
+
 if __name__ == '__main__':
-    print("Enter path to data file:")
-    path = input()
-    # path = "data/cos/data1.csv"
+    # print("Enter path to data file:")
+    # path = input()
 
-    f = open(path)
-    x = f.readline().split(',')
-    f.close()
-    x.pop(0)
+    anal_path = "data/soliton_up/1e3.0_dataAnalytic.csv"
 
-    t = np.loadtxt(path, delimiter=',', skiprows=1, usecols=(1,))
-    data = np.loadtxt(path, delimiter=',', skiprows=1, usecols=range(1,1001))
+    lw03_path = "data/soliton_up/1e0.3_dataLW.csv"
+    kir03_path = "data/soliton_up/1e0.3_dataKIR.csv"
 
-    # print(data[2])
+    lw06_path = "data/soliton_up/1e0.6_dataLW.csv"
+    kir06_path = "data/soliton_up/1e0.6_dataKIR.csv"
+
+    lw10_path = "data/soliton_up/1e1.0_dataLW.csv"
+    kir10_path = "data/soliton_up/1e1.0_dataKIR.csv"
+
+    lw13_path = "data/soliton_up/1e1.3_dataLW.csv"
+    kir13_path = "data/soliton_up/1e1.3_dataKIR.csv"
+
+    lw16_path = "data/soliton_up/1e1.6_dataLW.csv"
+    kir16_path = "data/soliton_up/1e1.6_dataKIR.csv"
+
+    lw20_path = "data/soliton_up/1e2.0_dataLW.csv"
+    kir20_path = "data/soliton_up/1e2.0_dataKIR.csv"
+
+    lw23_path = "data/soliton_up/1e2.3_dataLW.csv"
+    kir23_path = "data/soliton_up/1e2.3_dataKIR.csv"
+
+    lw26_path = "data/soliton_up/1e2.6_dataLW.csv"
+    kir26_path = "data/soliton_up/1e2.6_dataKIR.csv"
+
+    lw30_path = "data/soliton_up/1e3.0_dataLW.csv"
+    kir30_path = "data/soliton_up/1e3.0_dataKIR.csv"
+
+    lw33_path = "data/soliton_up/1e3.3_dataLW.csv"
+    kir33_path = "data/soliton_up/1e3.3_dataKIR.csv"
+
+    lw36_path = "data/soliton_up/1e3.6_dataLW.csv"
+    kir36_path = "data/soliton_up/1e3.6_dataKIR.csv"
+
+    data_arr = {
+        'analytic': FuncGrid(anal_path),
+        'lw03': FuncGrid(lw03_path),
+        'kir03': FuncGrid(kir03_path),
+        'lw06': FuncGrid(lw06_path),
+        'kir06': FuncGrid(kir06_path),
+        'lw10': FuncGrid(lw10_path),
+        'kir10': FuncGrid(kir10_path),
+        'lw13': FuncGrid(lw13_path),
+        'kir13': FuncGrid(kir13_path),
+        'lw16': FuncGrid(lw16_path),
+        'kir16': FuncGrid(kir16_path),
+        'lw20': FuncGrid(lw20_path),
+        'kir20': FuncGrid(kir20_path),
+        'lw23': FuncGrid(lw23_path),
+        'kir23': FuncGrid(kir23_path),
+        'lw26': FuncGrid(lw26_path),
+        'kir26': FuncGrid(kir26_path),
+        # 'lw30': FuncGrid(lw30_path),
+        # 'kir30': FuncGrid(kir30_path),
+        # 'lw33': FuncGrid(lw33_path),
+        # 'kir33': FuncGrid(kir33_path),
+        # 'lw36': FuncGrid(lw36_path),
+        # 'kir36': FuncGrid(kir36_path),
+    }
+
+    step_up_show()
+
+    l1_lw, linf_lw, l2_lw, times_lw = calc_dif(data_arr['analytic'], {
+        'lw10': data_arr['lw10'],
+        'lw13': data_arr['lw13'],
+        'lw16': data_arr['lw16'],
+        'lw20': data_arr['lw20'],
+        'lw23': data_arr['lw23'],
+        # 'lw26': data_arr['lw26'],
+        # 'lw30': data_arr['lw30'],
+        # 'lw33': data_arr['lw33'],
+        # 'lw36': data_arr['lw36'],
+    })
+    l1_kir, linf_kir, l2_kir, times_kir = calc_dif(data_arr['analytic'], {
+        'kir10': data_arr['kir10'],
+        'kir13': data_arr['kir13'],
+        'kir16': data_arr['kir16'],
+        'kir20': data_arr['kir20'],
+        'kir23': data_arr['kir23'],
+        # 'kir26': data_arr['kir26'],
+        # 'kir30': data_arr['kir30'],
+        # 'kir33': data_arr['kir33'],
+        # 'kir36': data_arr['kir36'],
+    })
+
+    dif_lw, times_lw1, pos_lw = calc_dif_point(data_arr['analytic'], {
+        'lw03': data_arr['lw03'],
+        # 'lw06': data_arr['lw06'],
+        'lw10': data_arr['lw10'],
+        'lw13': data_arr['lw13'],
+        'lw16': data_arr['lw16'],
+        'lw20': data_arr['lw20'],
+        'lw23': data_arr['lw23'],
+        # 'lw26': data_arr['lw26'],
+        # 'lw30': data_arr['lw30'],
+        # 'lw33': data_arr['lw33'],
+        # 'lw36': data_arr['lw36'],
+    })
+
+    dif_kir, times_kir1, pos_kir = calc_dif_point(data_arr['analytic'], {
+        'kir03': data_arr['kir03'],
+        # 'kir06': data_arr['kir06'],
+        'kir10': data_arr['kir10'],
+        'kir13': data_arr['kir13'],
+        'kir16': data_arr['kir16'],
+        'kir20': data_arr['kir20'],
+        'kir23': data_arr['kir23'],
+        # 'kir26': data_arr['kir26'],
+        # 'kir30': data_arr['kir30'],
+        # 'kir33': data_arr['kir33'],
+        # 'kir36': data_arr['kir36'],
+    })
+
+    # plt.plot(times_lw, np.log(l1_lw['lw10']), 'b')
+    # plt.plot(times_lw, np.log(l1_lw['lw13']), 'b')
+    # plt.plot(times_lw, np.log(l1_lw['lw16']), 'b')
+    # plt.plot(times_lw, np.log(l1_lw['lw20']), 'b')
+    #
+    # plt.plot(times_kir, np.log(l1_kir['kir10']), 'r')
+    # plt.plot(times_kir, np.log(l1_kir['kir13']), 'r')
+    # plt.plot(times_kir, np.log(l1_kir['kir16']), 'r')
+    # plt.plot(times_kir, np.log(l1_kir['kir20']), 'r')
+
+    pos = 1
+    time = 1
+
+    kir_dif = [
+        dif_kir['kir03'][time][pos],
+        # dif_kir['kir06'][time][pos],
+        dif_kir['kir10'][time][pos],
+        dif_kir['kir13'][time][pos],
+        dif_kir['kir16'][time][pos],
+        dif_kir['kir20'][time][pos],
+        dif_kir['kir23'][time][pos],
+        # dif_kir['kir26'][time][pos],
+        # dif_kir['kir30'][time][pos],
+        # dif_kir['kir33'][time][pos],
+        # dif_kir['kir36'][time][pos]
+    ]
+
+    lw_dif = [
+        dif_lw['lw03'][time][pos],
+        # dif_lw['lw06'][time][pos],
+        dif_lw['lw10'][time][pos],
+        dif_lw['lw13'][time][pos],
+        dif_lw['lw16'][time][pos],
+        dif_lw['lw20'][time][pos],
+        dif_lw['lw23'][time][pos],
+        # dif_lw['lw26'][time][pos],
+        # dif_lw['lw30'][time][pos],
+        # dif_kir['kir33'][time][pos],
+        # dif_kir['kir36'][time][pos]
+    ]
+
+    time = 0.1
+    # i_lw = np.where(times_lw == 0.1)
+    # i_kir = np.where(times_kir == 0.1)
+    i_lw = i_kir = 10
+    kir_l1 = [
+        l1_kir['kir10'][i_kir],
+        l1_kir['kir13'][i_kir],
+        l1_kir['kir16'][i_kir],
+        l1_kir['kir20'][i_kir],
+        l1_kir['kir23'][i_kir],
+        # l1_kir['kir26'][i_kir],
+        # l1_kir['kir30'][i_kir],
+        # l1_kir['kir33'][i_kir],
+        # l1_kir['kir36'][i_kir],
+    ]
+
+    kir_l2 = [
+        l2_kir['kir10'][i_kir],
+        l2_kir['kir13'][i_kir],
+        l2_kir['kir16'][i_kir],
+        l2_kir['kir20'][i_kir],
+        l2_kir['kir23'][i_kir],
+        # l2_kir['kir26'][i_kir],
+        # l2_kir['kir30'][i_kir],
+        # l2_kir['kir33'][i_kir],
+        # l2_kir['kir36'][i_kir],
+    ]
+
+    kir_linf = [
+        linf_kir['kir10'][i_kir],
+        linf_kir['kir13'][i_kir],
+        linf_kir['kir16'][i_kir],
+        linf_kir['kir20'][i_kir],
+        linf_kir['kir23'][i_kir],
+        # linf_kir['kir26'][i_kir],
+        # linf_kir['kir30'][i_kir],
+        # linf_kir['kir33'][i_kir],
+        # linf_kir['kir36'][i_kir],
+    ]
+    #
+    lw_l1 = [
+        l1_lw['lw10'][i_lw],
+        l1_lw['lw13'][i_lw],
+        l1_lw['lw16'][i_lw],
+        l1_lw['lw20'][i_lw],
+        l1_lw['lw23'][i_lw],
+        # l1_lw['lw26'][i_lw],
+        # l1_lw['lw30'][i_lw],
+        # l1_lw['lw33'][i_lw],
+        # l1_lw['lw36'][i_lw],
+    ]
+
+    lw_l2 = [
+        l2_lw['lw10'][i_lw],
+        l2_lw['lw13'][i_lw],
+        l2_lw['lw16'][i_lw],
+        l2_lw['lw20'][i_lw],
+        l2_lw['lw23'][i_lw],
+        # l2_lw['lw26'][i_lw],
+        # l2_lw['lw30'][i_lw],
+        # l2_lw['lw33'][i_lw],
+        # l2_lw['lw36'][i_lw],
+    ]
+
+    lw_linf = [
+        linf_lw['lw10'][i_lw],
+        linf_lw['lw13'][i_lw],
+        linf_lw['lw16'][i_lw],
+        linf_lw['lw20'][i_lw],
+        linf_lw['lw23'][i_lw],
+        # linf_lw['lw26'][i_lw],
+        # linf_lw['lw30'][i_lw],
+        # linf_lw['lw33'][i_lw],
+        # linf_lw['lw36'][i_lw],
+    ]
+    #
+    fineness = [2, 10, 20, 50, 100, 200]
+    #
+    # print(np.log(fineness), np.log(kir_l1), np.log(lw_l1), np.log(kir_l2), np.log(lw_l2), np.log(kir_linf), np.log(lw_linf))
+    print(np.log(fineness), np.log(kir_dif), np.log(lw_dif))
+
+    plt.plot(np.log(fineness), np.log(kir_dif), 'r')
+    plt.plot(np.log(fineness), np.log(lw_dif), 'b')
+    #
+    # plt.plot(np.log(fineness), np.log(kir_l2), 'r^')
+    # plt.plot(np.log(fineness), np.log(lw_l2), 'b^')
+    #
+    # plt.plot(np.log(fineness), np.log(kir_linf), 'rs')
+    # plt.plot(np.log(fineness), np.log(lw_linf), 'bs')
+
     plt.show()
